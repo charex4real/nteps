d1783d99cfaddcf38b1888e75947cf779bd3adbe
Charles Inyang <charex4real@gmail.com>
2019-06-01 15:33:30 +0100

    categories view edited

--- resources/views/themes/themeone/views/common/categories.blade.php
+++ resources/views/themes/themeone/views/common/categories.blade.php
@@ -6,27 +6,49 @@
   <div class="collapse navbar-collapse" id="navbar-categories">
 
     <ul class="navbar-nav flex-column">
-     @foreach($result['commonContent']['categories'] as $categories_data)
+    
      
       <li class="nav-item dropdown">
-        <a href="{{ URL::to('/shop')}}?category={{$categories_data->slug}}" class="nav-link dropdown-toggle">
-          <img class="img-fuild" src="{{asset('').$categories_data->icon}}">{{$categories_data->name}} @if(count($categories_data->sub_categories)>0) <i class="fa fa-angle-right " aria-hidden="true"></i> @endif
+        <a href="#" class="nav-link dropdown-toggle">
+          Shoe  <i class="fa fa-angle-right " aria-hidden="true"></i> 
         </a>
         
-        @if(count($categories_data->sub_categories)>0)
         <ul class="dropdown-menu multi-level">
-        	@foreach($categories_data->sub_categories as $sub_categories_data)            
+                
             <li class="dropdown-submenu">
-              <a  class="dropdown-item" tabindex="-1" href="{{ URL::to('/shop')}}?category={{$sub_categories_data->sub_slug}}">
-                <img class="img-fuild" src="{{asset('').$sub_categories_data->sub_icon}}">
-                {{$sub_categories_data->sub_name}}
+              <a  class="dropdown-item" tabindex="-1" href="#">
+                
+               Men's Shoe
+               <i class="fa fa-angle-right " aria-hidden="true"></i>
+              </a> 
+                <ul class="dropdown-menu multi-level">
+                  <li class="dropdown-submenu">
+                    <a  class="dropdown-item" tabindex="-1" href="#">
+                      
+                     Men's Shoe Child1
+                     <i class="fa fa-angle-right " aria-hidden="true"></i>
+                    </a>              
+                  </li> 
+                  <li class="dropdown-submenu">
+                    <a  class="dropdown-item" tabindex="-1" href="#">
+                      
+                    Men's Shoe Child2
+                    </a>              
+                  </li>            
+                </ul>             
+            </li> 
+            <li class="dropdown-submenu">
+              <a  class="dropdown-item" tabindex="-1" href="#">
+                
+              Women's Shoe
               </a>              
-            </li>            
-            @endforeach 
+            </li> 
+                       
+          
           </ul>
-          @endif
+        
         </li>
-        @endforeach
+       
     </ul>
   </div>
 </nav>
